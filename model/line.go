79f84cb00955d6f9c65757ddbf03e417be37c0ce
NiheiK <niheike1818@gmail.com>
2016-11-23 16:39:21 +0900

    LINEのPushを実装した

--- model/line.go
+++ model/line.go
@@ -2,12 +2,16 @@ package model
 
 import (
 	"github.com/line/line-bot-sdk-go/linebot"
-	"log"
 )
 
 func PushArticles(line *linebot.Client, articles []Article, users []User, max int) error {
 	// 配列articlesを、配列usersのlineIDに配信。ただし、最大件数はmaxとする
-	log.Println(articles)
-	log.Println(users)
+	for _, u := range users {
+		for _, a := range articles {
+			if _, err := line.PushMessage(u.LineID, linebot.NewTextMessage("【Group】\n"+a.Group.Name+"\n【Subject】\n"+a.Subject+"\n【Body】\n"+a.Body+"\nDate:"+a.SendDate.String())).Do(); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
